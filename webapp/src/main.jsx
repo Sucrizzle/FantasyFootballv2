124124c432a8ef40b100226251e85f4eb3d46a0d
Sucrizzle <sucrizzle@gmail.com>
2026-08-21 07:20:39 -0400

    Minor typo change

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
 import './index.css'
 import App from './App.jsx'
-import amplify_config from './amplifyconfig.json'
+import amplify_config from './amplify_config.json'
 
 // Call Amplify config
 Amplify.configure(amplify_config);
