172a23167ac46e1c4cc378a2dc7219e1367fb82c
Sucrizzle <sucrizzle@gmail.com>
2026-09-07 11:54:39 -0400

    finishing team per season

--- Lambda/Gold/queries/fact_team_stats_season.sql
+++ Lambda/Gold/queries/fact_team_stats_season.sql
@@ -3,14 +3,22 @@ select
 , ftsw.team
 , ds.game_category
 , count(ftsw.game_id) as games_played
-, sum(ftsw.score) as total_points
-, try_cast(total_points / games_played as decimal(10,2)) as points_per_game
-, sum(case when ftsw.team_category = 'Home' then ftsw.score end) as points_home
-, try_cast(points_home / sum(case when ftsw.team_category = 'Home' and not ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as points_home_per_game
-, sum(case when ftsw.team_category = 'Away' then ftsw.score end) as points_away
-, try_cast(points_away / sum(case when ftsw.team_category = 'Away' and not ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as points_away_per_game
-, sum(case when ds.is_neutral_location = 'Y' then ftsw.score end) as points_neutral
-, try_cast(points_neutral / sum(case when ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as points_neutral_per_game
+, sum(ftsw.score) as season__pts
+, try_cast(season__pts / games_played as decimal(10,2)) as pts_per_game
+, sum(case when ftsw.team_category = 'Home' then ftsw.score end) as pts_home
+, try_cast(pts_home / sum(case when ftsw.team_category = 'Home' and not ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as pts_home_pg
+, sum(case when ftsw.team_category = 'Away' then ftsw.score end) as pts_away
+, try_cast(pts_away / sum(case when ftsw.team_category = 'Away' and not ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as pts_away_pg
+, sum(case when ds.is_neutral_location = 'Y' then ftsw.score end) as pts_neutral
+, try_cast(pts_neutral / sum(case when ds.is_neutral_location then 1 else 0 end) as decimal(10,2)) as pts_neutral_pg
+, sum(def_fpts) as season_def_fpts
+, try_cast(season_def_fpts / games_played as decimal(10,2)) as def_fpts_pg
+, sum(st_fpts) as season_st_fpts
+, try_cast(season_st_fpts / games_played as decimal(10,2)) as st_fpts_pg
+, sum(pts_allowed_fpts) as season_pts_allowed_fpts
+, try_cast(season_pts_allowed_fpts / games_played as decimal(10,2)) as pts_allowed_fpts_pg
+, sum(dst_fpts) as season_dst_fpts
+, try_cast(season_dst_fpts / games_played as decimal(10,2)) as dst_fpts_pg
 FROM read_parquet('${bucket}/gold/facts/fact_team_stats_weekly.parquet', union_by_name = true) ftsw
 join read_parquet('${bucket}/gold/dimensions/dim_schedule.parquet', union_by_name = true) ds
   on ftsw.game_id = ds.game_id
